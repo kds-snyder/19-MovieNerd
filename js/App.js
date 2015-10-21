2c9260bd0c7e61319fe6b93069f85482a4944b2a
Karen Snyder <kds_snyder@yahoo.com>
2015-10-20 18:58:07 -0700

    Fixes, including initializing filter string

--- js/App.js
+++ js/App.js
@@ -5,7 +5,8 @@ function AppViewModel() {
   self.movies = ko.observableArray();
   self.failure = ko.observable();
   self.errorMessage = ko.observable();
-  self.filterString = ko.observable();
+  self.filterString = ko.observable('');
+  self.filteringTV = ko.observable();
 
 
   self.search = function() {
@@ -27,21 +28,51 @@ function AppViewModel() {
       }
 
     });
+
+    // If filtering TV shows, search for episode data;
+    //  it may not exist
+    if (self.filteringTV()) {
+      $.getJSON( "http://omdbapi.com/?s=" +
+                    self.searchInput() + '&type=episode',
+                                                function( data ) {
+          // If episode data was found, push to the movies array
+          //  and initialize failure indicator & error message
+          if (data.Search) {
+
+            for (var i = 0; i < data.Search.length; ++i) {
+              var currentShow = data.Search[i];
+              self.movies.push ({
+                Title: ko.observable(currentShow.Title),
+                Year: ko.observable(currentShow.Year),
+                imdbID: ko.observable(currentShow.imdbID),
+                Type: ko.observable(currentShow.Type),
+                Poster: ko.observable(currentShow.Poster)
+              });
+            }
+
+            self.failure(false);
+            self.errorMessage('');
+          }
+      });
+    }
   };
 
   // Filter for all
   self.filterAll = function() {
     self.filterString('');
+    self.filteringTV(false);
   };
 
   // Filter for movies
   self.filterMovies = function() {
     self.filterString('&type=movie');
+    self.filteringTV(false);
   };
 
   // Filter for TV shows
   self.filterTV = function() {
     self.filterString('&type=series');
+    self.filteringTV(true);
   };
 
 
